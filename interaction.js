c66dd12560b46fa8dbc4f793126373fe56b30056
Michael Crawford <mcrawbeast35@gmail.com>
2020-03-04 13:53:56 -0600

    Updated home page and added javascript

---
+++ interaction.js
@@ -0,0 +1,21 @@
+console.log('INFO: Reading index.js');
+
+console.log('INFO: Getting access to elements');
+
+const inputElementRadius = document.getElementById("radius")
+const inputElementResult = document.getElementById("result")
+const btnElement = document.getElementById("calc");
+
+console.log('INFO: Configuring event handlers');
+
+btnElement.onclick = async function ()
+{
+  const textRad = inputElementRadius.value;
+  const textResult = inputElementResult.value;
+  const Radius = parseFloat(textRad);
+
+  const Result = (4/3) * Math.PI * (Radius * Radius * Radius);
+  inputElementResult.innerHTML = "Result: " + Result;
+}
+
+console.log('INFO: Done loading, waiting for an event'); 
